c18a0548449c0f2905eb14a3a2ef9e6054cc5ed1
zongkaili <zongkaili@idealsee.cn>
2017-08-26 23:24:17 +0800

    调整投资理财和实物理财界面的ui

--- app/src/main/java/com/yixun/pettyloan/ui/fragment/EntityFragment.java
+++ app/src/main/java/com/yixun/pettyloan/ui/fragment/EntityFragment.java
@@ -80,14 +80,11 @@ public class EntityFragment extends BaseSupportFragment {
     @Override
     protected void initView() {
         TranslucentBarManager translucentBarManager = new TranslucentBarManager(this);
-        translucentBarManager.translucent(this, rootView, R.color.blue_medium);
+        translucentBarManager.translucent(this, rootView, R.color.bg_gray_medium);
         mBanner.setImageLoader(new GlideImageLoader());
-        List<String> urlList = new ArrayList<>();
-        urlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502364345401&di=c973f24fda4b0e74d9d233efd0a34dd3&imgtype=0&src=http%3A%2F%2Fimg3.duitang.com%2Fuploads%2Fitem%2F201608%2F10%2F20160810204650_8sQuF.jpeg");
-        urlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502364364886&di=48c237b56ad5126d60aac44b6d35c6ac&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1038163141%2C2794573076%26fm%3D214%26gp%3D0.jpg");
-        urlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502364345401&di=a137348f727c523a65936c60d6a6b6cb&imgtype=0&src=http%3A%2F%2Fimg3.duitang.com%2Fuploads%2Fitem%2F201605%2F26%2F20160526175959_xRWTH.thumb.700_0.jpeg");
-        urlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502364345401&di=f101d8d3cc8abfed11a2ad6584a30df4&imgtype=0&src=http%3A%2F%2Fphotocdn.sohu.com%2F20160907%2FImg467862638.jpg");
-        urlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502364345400&di=9812520307a97a517568e690257da493&imgtype=0&src=http%3A%2F%2Fimg.ladywu.net%2F2017%2F0704%2F20170704021437678.jpg");
+        List<Object> urlList = new ArrayList<>();
+        urlList.add(R.drawable.pic_invest_banner);
+        urlList.add(R.drawable.pic_banner2);
         mBanner.setImages(urlList).start();
 
         List<Product> goodList = new ArrayList<>();
--- app/src/main/java/com/yixun/pettyloan/ui/fragment/InvestFragment.java
+++ app/src/main/java/com/yixun/pettyloan/ui/fragment/InvestFragment.java
@@ -12,14 +12,14 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.kelin.translucentbar.library.TranslucentBarManager;
 import com.yixun.pettyloan.R;
 import com.yixun.pettyloan.adapter.multitype.MultiTypeAdapter;
+import com.yixun.pettyloan.entity.BannerFeed;
+import com.yixun.pettyloan.entity.MainBannerItemViewBinder;
 import com.yixun.pettyloan.entity.Product;
 import com.yixun.pettyloan.entity.ProductItemViewBinder;
 import com.yixun.pettyloan.ui.base.BaseSupportFragment;
-import com.yixun.pettyloan.ui.widge.SpaceDecoration;
-import com.youth.banner.Banner;
+import com.yixun.pettyloan.ui.widge.SpaceDecorationExceptFirst;
 import com.youth.banner.loader.ImageLoaderInterface;
 
 import java.util.ArrayList;
@@ -37,8 +37,6 @@ public class InvestFragment extends BaseSupportFragment {
     Toolbar mToolbar;
     @BindView(R.id.rv_invest_content)
     RecyclerView mRecyclerView;
-    @BindView(R.id.banner_invest)
-    Banner mBanner;
     @BindView(R.id.srl_refresh)
     SwipeRefreshLayout mRefresh;
 
@@ -76,36 +74,6 @@ public class InvestFragment extends BaseSupportFragment {
 
     @Override
     protected void initView() {
-        TranslucentBarManager translucentBarManager = new TranslucentBarManager(this);
-        translucentBarManager.translucent(this, rootView, R.color.blue_medium);
-        bindBanner();
-    }
-
-    private void bindBanner() {
-        mBanner.setImageLoader(new GlideImageLoader());
-        List<String> mUrlList = new ArrayList<>();
-        mUrlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502364345401&di=c973f24fda4b0e74d9d233efd0a34dd3&imgtype=0&src=http%3A%2F%2Fimg3.duitang.com%2Fuploads%2Fitem%2F201608%2F10%2F20160810204650_8sQuF.jpeg");
-        mUrlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502364364886&di=48c237b56ad5126d60aac44b6d35c6ac&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1038163141%2C2794573076%26fm%3D214%26gp%3D0.jpg");
-        mUrlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502364345401&di=a137348f727c523a65936c60d6a6b6cb&imgtype=0&src=http%3A%2F%2Fimg3.duitang.com%2Fuploads%2Fitem%2F201605%2F26%2F20160526175959_xRWTH.thumb.700_0.jpeg");
-        mUrlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502364345401&di=f101d8d3cc8abfed11a2ad6584a30df4&imgtype=0&src=http%3A%2F%2Fphotocdn.sohu.com%2F20160907%2FImg467862638.jpg");
-        mUrlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502364345400&di=9812520307a97a517568e690257da493&imgtype=0&src=http%3A%2F%2Fimg.ladywu.net%2F2017%2F0704%2F20170704021437678.jpg");
-        mBanner.setImages(mUrlList).start();
-    }
-
-    private class GlideImageLoader implements ImageLoaderInterface {
-
-        @Override
-        public void displayImage(Context context, Object path, View imageView) {
-            Glide.with(context.getApplicationContext())
-                    .load(path)
-                    .crossFade()
-                    .into((ImageView) imageView);
-        }
-
-        @Override
-        public View createImageView(Context context) {
-            return new ImageView(context);
-        }
     }
 
     @Override
@@ -116,13 +84,21 @@ public class InvestFragment extends BaseSupportFragment {
 
     private void bindFeeds() {
         mFeedAdapter = new MultiTypeAdapter();
+        mFeedAdapter.register(BannerFeed.class, new MainBannerItemViewBinder());
         mFeedAdapter.register(Product.class, new ProductItemViewBinder(this));
-        mRecyclerView.addItemDecoration(new SpaceDecoration((int) getResources().getDimension(R.dimen.goods_margin)));
+        mRecyclerView.addItemDecoration(new SpaceDecorationExceptFirst((int) getResources().getDimension(R.dimen.goods_margin)));
         mRecyclerView.setAdapter(mFeedAdapter);
+        List<Object> urlList = new ArrayList<>();
+        urlList.add(R.drawable.pic_invest_banner);
+        urlList.add(R.drawable.pic_banner2);
         items = new ArrayList<>();
+        items.add(new BannerFeed(urlList));
         items.add(new Product("产品１", "30", "% + 0.32%"));
         items.add(new Product("产品2", "40", "% + 0.32%"));
         items.add(new Product("产品3", "50", "% + 0.32%"));
+        items.add(new Product("产品4", "30", "% + 0.32%"));
+        items.add(new Product("产品5", "40", "% + 0.32%"));
+        items.add(new Product("产品6", "50", "% + 0.32%"));
         mFeedAdapter.setItems(items);
         mFeedAdapter.notifyDataSetChanged();
     }
